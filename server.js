db91617c2ae0f8418f565288d732364e1d021d57
GabrielD <gdedi651@gmail.com>
2016-03-24 01:52:07 -0400

    add comments

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ app.get('/', function(req, res){
 io.on('connection', function(client){
 	console.log('client connected...');
 	
-	// JOIN event handler
+	// CLIENT -> SERVER:JOIN event handler
 	client.on('join', function(name){
+		client.nickname = name;
+		
 		// get a range of elements, in this case 0, -1 indicates ALL 
 		redisClient.lrange("messages", 0, -1, function(err, messages){
 			messages = messages.reverse(); // reverse so that they are emmited in most recent order
@@ -30,12 +32,12 @@ io.on('connection', function(client){
 		});
 	});
 	
-	// ANSWERS event handler
+	// CLIENT -> SERVER:ANSWERS event handler
 	client.on('answers', function(message, answer){
 		client.broadcast.emit('answer', message, answer);
 	});
 
-	// MESSAGES event handler
+	// CLIENT -> SERVER:MESSAGE event handler
 	client.on('message', function(message){
 		client.broadcast.emit('message', message);
 		console.log('message: ' + message);
