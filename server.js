5a3c042cccb6fa2711bc096794ae06cb941ca21d
GabrielD <gdedi651@gmail.com>
2016-03-23 23:22:36 -0400

    require redis

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 "use strict";
+// Redis DB
+var redis = require('redis');
+// Express with server functionality
 var	express = require('express');
 var app = express();
 var server = require('http').Server(app);
+// Sockets with real-time data
 var io = require('socket.io').listen(server);
 
 // Routing refers to determining how an application responds to a client request to a particular endpoint
@@ -13,20 +17,18 @@ app.get('/', function(req, res){
 io.on('connection', function(client){
 	console.log('client connected...');
 	
-	// listen for answers
+	client.on('join', function(name){
+		// 'socket.broadcast.emit' will send the message to all the other clients except the newly created connection
+		client.broadcast.emit();
+	});
+	
+	// ANSWERS event handler
 	client.on('answers', function(message, answer){
 		client.broadcast.emit('answer', message, answer);
 	});
 
-	// listen for messages
+	// MESSAGES event handler
 	client.on('message', function(message){
-		/* uncomment for 1 message per client
-
-		if (!client.question_asked) {
-			client.question_asked = true;
-			client.broadcast.emit('message', message);
-		}
-		*/
 		client.broadcast.emit('message', message);
 		console.log('message: ' + message);
 	});
