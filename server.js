d153273ac85bc575eb1ee5a2796f768ef4358050
GabrielD <gdedi651@gmail.com>
2016-03-30 03:25:57 -0400

    incorporate required modules and proper socket event handlers

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 "use strict";
-// Redis DB
+/* Redis DB
 var redis = require('redis');
-var redisClient = redis.createClient();
+var redisClient = redis.createClient(); */
+var chat_session = require('./chat-session');
 // Express with server functionality
 var	express = require('express');
 var app = express();
 var server = require('http').Server(app);
 // Sockets with real-time data
 var io = require('socket.io').listen(server);
+const PORT = 8080;
 
 // Routing refers to determining how an application responds to a client request to a particular endpoint
 app.get('/', function(req, res){
@@ -16,42 +18,41 @@ app.get('/', function(req, res){
 
 // Once a browser connects to localhost:8080, the 'connection' event will fire
 io.on('connection', function(client){
-	console.log('client connected...');
 	
 	// CLIENT -> SERVER:JOIN event handler
 	client.on('join', function(name){
-		client.nickname = name;
-		
-		// get a range of elements, in this case 0, -1 indicates ALL 
-		redisClient.lrange("messages", 0, -1, function(err, messages){
-			messages = messages.reverse(); // reverse so that they are emmited in most recent order
-			messages.forEach(function(message){
-				message = JSON.parse(message)
-				client.emit("messages", message.name + ": " + message.data);
-			});
+		chat_session.default.messages.forEach(function(message){
+			client.emit("message", message.name + ": " + message.data);
 		});
+		
+		client.nickname = name;
+		client.broadcast.emit("enter", "CHAT ALERT: " + name + " has entered the room");
+		// add new chatters to array in chat-session module
+		chat_session.default.users.push(client.nickname);
+		chat_session.default.count++; // update amount of chatters in the room
+		
+		console.log(client.nickname + " has joined!!");
 	});
 	
-	// CLIENT -> SERVER:ANSWERS event handler
-	client.on('answers', function(message, answer){
-		client.broadcast.emit('answer', message, answer);
-	});
-
-	// CLIENT -> SERVER:MESSAGE event handler
 	client.on('message', function(message){
-		client.broadcast.emit('message', message);
-		console.log('message: ' + message);
+		var nickname = client.nickname;
+		client.broadcast.emit("message", nickname + ": " + message); // send the message to all the other clients except the newly created connection
+		client.emit("me", nickname + ": " + message); // send message to self
+		storeMessage(nickname, message); // store the message in chat-session
+		console.log(nickname + ' said: ' + message); // used for terminal debugging
 	});
+	
+	//TODO: figure out usage of this function
+//	client.on('answers', function(message, answer){
+//		client.broadcast.emit('answer', message, answer);
+//	});
 });
 
-var storeMessage = function(name, data){
-	// converts value to a JSON string for storage in Redis
-	var message = JSON.stringify({name: name, data: data});
-	
-	redisClient.lpush("messages", message, function(err, response){
-		// always keeps newest 10 items in array
-		redisClient.ltrim("messages", 0, 9);
-	});
-};
-
-server.listen(8080);
+function storeMessage(name, data){
+	chat_session.default.messages.push({name: name, data: data}); // add message to end of array
+	if (chat_session.default.messages.length > 10) {
+		chat_session.default.messages.shift(); // if more than 10 messages long, remove the first one
+	}
+}
+		
+server.listen(PORT);
