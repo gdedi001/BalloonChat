5cc4736b7ce0af3b43c61698e049bd23b0bbc318
GabrielD <gdedi651@gmail.com>
2016-04-05 15:38:24 -0400

    move play function and change tabs event

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,23 @@
-// Create play function 
-//function play() {
-//    var audio = document.getElementById('audio');
-//    audio.play();
-//}
+// Create function that handles notification sound
+function play() {
+    var audio = document.getElementById('audio');
+    audio.play();
+}
 
 $(document).ready(function(){
+	$('#private').on('click', function(){
+		alert('Click on a user to send private message.');
+	});
 	
-//	// change room tabs
-//	$('.nav li').on('click', function(){
-//		$('.nav').find('.active').removeClass();
-//		$(this).addClass('active');
-//		defRoom = $(this).attr('id');;
-//		console.log(defRoom);
-//	});
-	
+	// change room tabs
+	$('.nav li').on('click', function(){
+			$('.nav').find('.active').removeClass();
+			$(this).addClass('active');
+			curRoom = $(this).attr('id');
+
+			server.emit('switch', curRoom); // tell the server the client has switched rooms
+
+			$('#messages').empty();
+			console.log(curRoom);
+	});	
 });
